f427db00687ed1cf8f204fdf9e3f0bcbc828b9d6
Subhajit <mandalsubhajit@gmail.com>
2018-05-15 08:09:30 +0530

    historical price data

---
+++ prices.py
@@ -0,0 +1,22 @@
+from pandas_datareader import data as pdr
+
+nifty500 = pd.read_csv('ind_nifty500list.csv')
+symbols = nifty500.Symbol.tolist()
+
+prices = []
+
+for sym in symbols:
+    print(sym)
+    sym = sym.replace('-', '_')
+    sym = sym.replace('&', '')
+    data = pdr.DataReader('NSE/'+sym, 'quandl',
+                          start=datetime.datetime(2005, 1, 1), 
+                          end=datetime.datetime(2018, 6, 1))
+    data.index.name = 'Date'
+    data.reset_index(inplace=True)
+    data['symbol'] = sym
+    prices.append(data)
+
+prices = pd.concat(prices, ignore_index=True)
+prices = prices[['symbol'] + [c for c in prices.columns if c != 'symbol']]
+prices.to_csv('prices.csv', index=False)
